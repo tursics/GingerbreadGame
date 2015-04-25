d2c922a2bf506cb77305ffe0f5bc0ebfa30eb59e
Thomas Tursics <thomas@tursics.de>
2015-04-26 00:56:43 +0200

    init version

---
+++ script/CInit.js
@@ -0,0 +1,36 @@
+﻿//--------------------------------
+//---         CInit.js         ---
+//--------------------------------
+
+CInit = new function()
+{
+	//------------------------
+	this.eventReady = function()
+	{
+		try {
+			MyMap.eventReady();
+			MyPlayer.eventReady();
+		} catch( e) { if( CConfig.debug) { alert( e); } }
+	};
+
+	//------------------------
+	this.eventResize = function()
+	{
+		try {
+			MyMap.eventResize();
+			MyPlayer.eventResize();
+		} catch( e) { if( CConfig.debug) { alert( e); } }
+	};
+
+	//------------------------
+};
+
+//----------------------------
+
+function wpGotoPage( pageName)
+{
+	$.mobile.changePage( "#" + pageName);
+}
+
+//----------------------------
+// eof
