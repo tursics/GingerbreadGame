80b33dfef9e57306ce4d11049c07a6b71cdcbdf1
Thomas Tursics <thomas@tursics.de>
2017-04-26 10:16:04 +0200

    added a foo file

---
+++ script/CStateFoo.js
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------------------------------
+// state to boot the system
+// ---------------------------------------------------------------------------------------
+
+function CStateBoot( inits, game)
+{
+	this.inits = inits;
+	this.game = game;
+
+	try {
+	} catch(e) {
+		console.error( 'CStateBoot not ready', e);
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+
+CStateBoot.prototype.preload = function()
+{
+	try {
+	} catch( e) {
+		if( CConfig.debug) {
+			console.error( 'CStateBoot preload error', e);
+		}
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+
+CStateBoot.prototype.create = function()
+{
+	try {
+	} catch( e) {
+		if( CConfig.debug) {
+			console.error( 'CStateBoot creation error', e);
+		}
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+
+CStateBoot.prototype.update = function()
+{
+	try {
+	} catch( e) {
+		if( CConfig.debug) {
+			console.error( 'CStateBoot update error', e);
+		}
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+// eof
